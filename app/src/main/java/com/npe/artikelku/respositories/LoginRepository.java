4a98d4c101fa0b7ee46d2291ad6729950792abb1
renaldy <renaldysab18@gmail.com>
2019-04-03 10:03:15 +0700

    login - add okhttp and trying to save date user to repo

--- app/src/main/java/com/npe/artikelku/respositories/LoginRepository.java
+++ app/src/main/java/com/npe/artikelku/respositories/LoginRepository.java
@@ -1,5 +1,15 @@
 package com.npe.artikelku.respositories;
 
-public class LoginRepository {
+import com.npe.artikelku.rest.ApiClient;
 
+public class LoginRepository {
+    public static LoginRepository instance;
+    private ApiClient mApiClient;
+
+    public static LoginRepository getInstance(){
+        if(instance == null){
+            instance = new LoginRepository();
+        }
+        return instance;
+    }
 }
